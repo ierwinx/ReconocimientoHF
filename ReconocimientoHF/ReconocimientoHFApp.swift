e828bc97a4bf6ae5003ed5c83a5881d2a747ef37
Eяw!и Gontier <ierwinx@me.com>
2022-09-08 16:54:31 -0500

    Initial Commit

---
+++ ReconocimientoHF/ReconocimientoHFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReconocimientoHFApp.swift
+//  ReconocimientoHF
+//
+//  Created by Erwin Luz León on 08/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ReconocimientoHFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
